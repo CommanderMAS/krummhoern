32c326eb79a988c530b6a3c038ef0f8b5ddb9b00
CommanderMAS <38735959+CommanderMAS@users.noreply.github.com>
2023-06-01 11:23:27 +0200

    Update frontend.blade.php

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -37,15 +37,11 @@
 
 <!-- ======= Top Bar ======= -->
 <section id="topbar" class="d-flex align-items-center">
-    <div class="container d-flex justify-content-center justify-content-md-between">
+    <div class="container justify-content-center justify-content-md-between">
         <div class="contact-info d-flex align-items-center">
-            <i class="bi bi-envelope d-flex align-items-center"><a
-                    href="mailto:#">verwaltung@deichacht-kruemmhoern.de</a></i>
-            <i class="bi bi-phone d-flex align-items-center ms-4"><span>0 49 23 - 91 11 0</span></i>
-        </div>
-        <div class="social-links d-none d-md-flex align-items-center">
-            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+            <i class="bi bi-envelope align-items-center"><a
+                    href="mailto:verwaltung@deichacht-krummhoern.de">verwaltung@deichacht-krummhoern.de</a></i>
+            <i class="bi bi-phone align-items-center ms-4"><a href="tel:+49492391110"><span>0 49 23 - 91 11 0</span></a> </i>
         </div>
     </div>
 </section>
